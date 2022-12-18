c642276f430b28656d8e6f8865e7cb697738d74a
Haripriya <haripriyas2024a@mca.ajce.in>
2022-12-18 21:10:23 +0530

    my first commit

---
+++ eg3.py
@@ -0,0 +1,3 @@
+cel=float(input("Enter the temperature in celsius"))
+fahrenheit=(cel*9/5)+32
+print("fahrenheit is :",fahrenheit)
---
+++ eg7.py
@@ -0,0 +1,5 @@
+
+ra=float(input("Enter the radius of a cylinder"))
+ht=int(input("Enter the height of a cylinder"))
+volume=3.14*ra*ra*ht
+print("volume is",volume)
